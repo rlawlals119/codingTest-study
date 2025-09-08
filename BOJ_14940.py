710126e331bdb3290fddf9fe92cb6d364a4d6edb
rlawlals119 <rlawlals170@naver.com>
2025-09-09 00:48:58 +0900

    BOJ_14940: 쉬운 최단거리

--- BOJ_14940.py
+++ BOJ_14940.py
@@ -1,7 +1,31 @@
+from collections import deque
+
 n, m = map(int, input().split())
 
 graph = []
 for _ in range(n):
     graph.append(list(map(int, input().split())))
 
-print(graph)
+direct = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+ans = [[-1 for _ in range(m)] for __ in range(n)]
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 0: ans[i][j] = 0
+        if graph[i][j] == 2:
+            q = deque([(i, j, 0)])
+            ans[i][j] = -1
+
+            while q:
+                x, y, dist = q.popleft()
+
+                for a, b in direct:
+                    dx = x + a
+                    dy = y + b
+
+                    if dx >= 0 and dx < n and dy >= 0 and dy < m and graph[dx][dy] != 0 and ans[dx][dy] == -1:
+                        ans[dx][dy] = dist + 1
+                        q.append((dx, dy, dist + 1))
+
+            ans[i][j] = 0
+for row in ans:
+    print(*row)                 # 공백 구분으로 int도 바로 출력

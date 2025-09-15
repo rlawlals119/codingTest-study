dd993d501d2592d1d9052e7a9a7d6ddb42407b51
rlawlals119 <rlawlals170@naver.com>
2025-09-15 23:49:37 +0900

    BOJ_1629: 분할정복 이용

--- BOJ_1629.py
+++ BOJ_1629.py
@@ -1,13 +1,11 @@
 a, b, c = map(int, input().split())
 
-num = a % c
-if b == 1: print(num)
-else:
-    pattern = []
-    for _ in range(b - 1):
-        num = num * a % c
-        if num in pattern: break
-        pattern.append(num)
-
-    if len(pattern) == 1: print(pattern[0])
-    else: print(pattern[(b - 1) % len(pattern) - 1])
+def mod_pow(a, b, c):
+    if b == 0:
+        return 1
+    if b % 2 == 0:
+        half = mod_pow(a, b // 2, c)
+        return half * half % c
+    else:
+        return (a * mod_pow(a, b - 1, c)) % c
+print(mod_pow(a, b, c))

d4508e915a8df109be78c9e41ba0f8a2caba94fd
rlawlals119 <rlawlals170@naver.com>
2025-09-25 17:29:46 +0900

    commit

--- BOJ_18291.py
+++ BOJ_18291.py
@@ -2,4 +2,10 @@ t = int(input())
 
 for _ in range(t):
     n = int(input())
+    if n == 1:
+        print(1)
+        continue
+    if n == 2:
+        print(1)
+        continue
     print(pow(2, n - 2, 10 ** 9 + 7))
